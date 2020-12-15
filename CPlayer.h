c5537d3fa10c018d17490cd0ba3067290fd8c2d5
tvminh19 <56433333+tvminh19@users.noreply.github.com>
2020-12-15 09:43:52 +0700

    update for Dien

--- CPlayer.h
+++ CPlayer.h
@@ -1,10 +1,25 @@
 #ifndef _CPLAYER_H_
 #define _CPLAYER_H_
+#include "CPosition.h"
 class CPlayer{
 private:
+    CPosition position;
+    bool isDead=false;
+    char **shape;
+    char **emptyShape;
+    int width=5, height=3;
 public:
     CPlayer();
-    
+    void up();
+    void down();
+    void left();
+    void right();
+    bool getDeadStt(); //check is dead?
+    void setDeadStt(); 
+    int getX();
+    int getY();
+    void sound();
+    bool crash(CPosition pos, int _width, int _height);
 };
 
 #endif // _CPLAYER_H_
