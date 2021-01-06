180f9c19cb57187fd1b259e0a9fcc4663e7131e3
unitoflazy <tntdat19@apcs.vn>
2021-01-07 01:52:20 +0700

    add file cpp menu

--- zmain.cpp
+++ zmain.cpp
@@ -13,9 +13,10 @@ int main() {
     case 2:
         menu.rank();
         break;
-    case 3:
+    /*case 3:
         menu.music();
-        break;
+        break;*/
+    //No need
     case 4:
         menu.exit_game();
         break;
