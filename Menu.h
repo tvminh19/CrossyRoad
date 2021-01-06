b5aac1f5adbe27d27438dcf0e4d6c0828eb20583
TranHaiAnhDien <keepgoing0901@gmail.com>
2021-01-06 17:03:16 +0700

    fix more ranking click

--- Menu.h
+++ Menu.h
@@ -735,7 +735,23 @@ public:
 			window->display();
 		}
 
-		return this->drawMenu();
+		switch (this->drawMenu()){
+				case 0:
+					return newGame();
+					break;
+				case 1:
+					return loadGame();
+					break;
+				case 2:
+					return rank();
+					break;
+				case 3:
+					return music();
+					break;
+				case 4:
+					return exit_game();
+					break;
+				}
 	}
 
 	template<class T>
